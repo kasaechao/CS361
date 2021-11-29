27c1b32cb0e9df1ebfa49ef91b55e524c642bb50
kasaechao <you@example.com>
2021-11-29 10:44:06 -0800

    finished

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,72 +1,54 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 const Trailer = (props) => {
-  
   return (
     <div>
-      <iframe src={props.trailer}></iframe>
+      <iframe src={props.trailer} width="50%" height="100%"></iframe>
     </div>
   )
 }
 
 const Result = (props) => {
 		
-		const [serviceData, setFakeData] = useState("")
+		const [serviceData, setServiceData] = useState("")
     const [showTrailerBool, setShowTrailer] = useState(false)
     const [description, setDescription] = useState("")
     const [filmImage, setfilmImage] = useState("")
     const [buttonText, setButtonText] = useState("Recommend me a Movie!")
     
-		const [fakeWiki, setFakeWiki] = useState({})
-		const [fakeReview, setFakeReview] = useState({})
-
-
 		const response = () => {
 			// setFakeData({result:'future microservice video extractor to return movie trailer'})
       axios.post('https://movie-service-cs361.herokuapp.com/', 
       {
-        "yearRange":["2000","2010"],
+        "yearRange":["2021","2021"],
         "genres":["comedy"],
-        "rating":"7"
+        "rating":"4"
       })
         .then((res) => {
-          setFakeData(res.data.trailer)
+          setServiceData(res.data.trailer)
           setDescription(res.data.overview)
-          
-          // setfilmImage(`https://image.tmdb.org/t/p/original/${res.data.backdrop_path}`)
           setfilmImage(`https://image.tmdb.org/t/p/original/${res.data.poster_path}`)
           setButtonText('Click here again to recommend another movie!')
-
           setShowTrailer(true)
         })
-		}
-
-		const responseWiki = () => {
-			setFakeWiki({result:'future microservice to link wiki info page'})
-		}
-
-		const responseReview = () => {
-			setFakeReview({result:'future service to provide a link movie review'})
 		}
 
     const stepBoxStyle = {
         fontStyle: 'italic',
         fontSize: 16,
-        // margin: 'auto',
         display: 'flex',
 				alignSelf: 'auto',
-				// flexWrap: 'wrap',
     }
 
     const mainBoxStyle = {
         fontStyle: 'italic',
         fontSize: 16,
         borderStyle: 'solid',
-        width: '30%',
+        width: '70%',
         margin: 'auto',
+        height: '50%'
     }
-
 
 		const buttonLayoutStyle = {
 			paddingLeft: '30%',
@@ -88,11 +70,6 @@ const Result = (props) => {
 			fontSize: '16px',
 		}
 
-    const textStyle = {
-        margin: 'auto',
-        padding: '10px',
-    }
-
     const imageStyle = { 
         width: '225px',
         height: '400px',
@@ -100,30 +77,24 @@ const Result = (props) => {
     }
 
     return (
+      // The results will only render once the button is clicked. The boolean logic starts on line 92
         <div>
-        <div style={buttonLayoutStyle}>
-          <button style={buttonStyle} onClick={response}>{buttonText}</button>
-        </div>
-          {showTrailerBool ?         <div style={mainBoxStyle}>
-					<div style={stepBoxStyle}>
-						<div><img style={imageStyle} src={showTrailerBool ? filmImage : null} alt="" /></div>
-						<div style={{padding:'10px'}}>
-								<h3>Description</h3>
-								<p>{showTrailerBool ? description : null}</p>
-                <h3>Trailer</h3>
-                {showTrailerBool  ? <Trailer trailer={serviceData}/> : null}
-
-						</div>
-
-        </div>
-					<div style={buttonLayoutStyle}>
-						{/* <button style={buttonStyle} onClick={responseWiki}>Reviews</button>
-						<pre>{JSON.stringify(fakeWiki)}</pre>
-						<button style={buttonStyle} onClick={responseReview}>Wiki (Will call text grabber microservice provided by me)</button>
-						<pre>{JSON.stringify(fakeReview)}</pre> */}
-					</div>
-        </div>
-        : null}        </div>
+          <div style={buttonLayoutStyle}>
+            <button style={buttonStyle} onClick={response}>{buttonText}</button>
+          </div>
+            {showTrailerBool ? <div style={mainBoxStyle}>
+              <div style={stepBoxStyle}>
+                <div><img style={imageStyle} src={showTrailerBool ? filmImage : null} alt="" /></div>
+                <div style={{padding:'5px'}}>
+                    <h3>Description</h3>
+                    <p>{showTrailerBool ? description : null}</p>
+                    <h3>Trailer</h3>
+                    {showTrailerBool  ? <Trailer trailer={serviceData}/> : null}
+                </div>
+              </div>
+            <div style={buttonLayoutStyle}></div>
+            </div>
+        : null} </div>
     )
 }
 
